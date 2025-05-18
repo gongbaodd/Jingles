575bf7e2c56b3cf290dcb01803b08eb08d24ddd3
gongbaodd@ProArt <gongbaodd@qq.com>
2025-05-18 21:09:59 +0300

    init VR project

--- xr-project/src/Scene.tsx
+++ xr-project/src/Scene.tsx
@@ -1,4 +1,5 @@
-import { ArcRotateCamera, Color3, HemisphericLight, MeshBuilder, Scene, StandardMaterial, Texture, Vector3, type Engine } from "@babylonjs/core";
+import { ArcRotateCamera, EquiRectangularCubeTexture, HemisphericLight, ImportMeshAsync, MeshBuilder, PBRMaterial, Scene, Texture, Vector3, type Engine } from "@babylonjs/core";
+// import "@babylonjs/materials";
 
 export async function createScene(engine: Engine) {
     const scene = new Scene(engine);
@@ -8,7 +9,7 @@ export async function createScene(engine: Engine) {
         -Math.PI / 4,
         Math.PI / 2.5,
         200,
-        new Vector3(0, 100, 0),
+        new Vector3(0, 0, 0),
         scene
     );
 
@@ -18,32 +19,42 @@ export async function createScene(engine: Engine) {
     const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
     light.intensity = 0.7;
 
-    MeshBuilder.CreateGround(
-        "ground",
-        { width: 100, height: 100 },
-        scene
-    );
-
-    // scene.fogMode = Scene.FOGMODE_EXP; // Or FOGMODE_EXP2, FOGMODE_LINEAR
-    // scene.fogColor = new Color3(0.2, 0.2, 0.3); // Example fog color
-    // scene.fogDensity = 0.01; // For exponential fog modes
-    // scene.fogStart = 20.0; // For linear fog mode
-    // scene.fogEnd = 2000.0;   // For linear fog mode
-
-
     loadSky(scene);
+    LoadPointCloud(scene);
 
     return scene;
 }
 
 function loadSky(scene: Scene) {
-    const skybox = MeshBuilder.CreateSphere("skyBox", { diameter: 1000 }, scene);
-    const material = new StandardMaterial("skyBox", scene);
-    const texture = new Texture("./images/sky.jpg");
-    texture.coordinatesMode = Texture.SKYBOX_MODE;
-    texture.vScale = -1;
+    const eqTexture = new EquiRectangularCubeTexture(
+        "./images/sky.jpg",
+        scene,
+        1024,
+    );
+    eqTexture.coordinatesMode = Texture.SKYBOX_MODE;
 
-    material.backFaceCulling = false;
-    material.diffuseTexture = texture;
-    skybox.material = material;
+    const hdrSkybox = MeshBuilder.CreateBox("hdrSkyBox", { size: 2000 }, scene);
+    const hdrSkyboxMaterial = new PBRMaterial("skyBox", scene);
+    hdrSkyboxMaterial.backFaceCulling = false;
+    hdrSkyboxMaterial.reflectionTexture = eqTexture.clone();
+    hdrSkyboxMaterial.reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE;
+    hdrSkyboxMaterial.microSurface = 1.0;
+    hdrSkyboxMaterial.disableLighting = false;
+    hdrSkybox.material = hdrSkyboxMaterial;
+    hdrSkybox.infiniteDistance = true;
+
+    scene.imageProcessingConfiguration.exposure = 0.6;
+    scene.imageProcessingConfiguration.contrast = 3.0;
 }
+
+async function LoadPointCloud(scene: Scene) {
+    const result = await ImportMeshAsync("models/logo.ply", scene);
+
+    const mesh = result.meshes[0];
+    mesh.position.y = -50.0;
+    mesh.rotation.z = Math.PI;
+    mesh.rotation.y = -280/360 * Math.PI;
+    mesh.scaling.x = 100;
+    mesh.scaling.y = 100;
+    mesh.scaling.z = 100;
+  }
--- xr-project/src/main.tsx
+++ xr-project/src/main.tsx
@@ -1,6 +1,10 @@
 import { Engine } from '@babylonjs/core';
 import './index.css'
 import { createScene } from './Scene';
+import "@babylonjs/inspector";
+import { registerBuiltInLoaders } from "@babylonjs/loaders/dynamic"
+
+registerBuiltInLoaders();
 
 const canvas = document.querySelector<HTMLCanvasElement>("#canvas")!;
 
@@ -20,6 +24,29 @@ const engine = new Engine(canvas, true, {
 });
 let scene = await createScene(engine);
 engine.runRenderLoop(() => scene.render());
+
+
+document.getElementById("xr-button")!.addEventListener("click", async () => {
+  if (!scene) return;
+
+  try {
+    const xrHelper = await scene.createDefaultXRExperienceAsync({
+      uiOptions: {
+        sessionMode: "immersive-vr",
+      },
+      optionalFeatures: true,
+    });
+
+    // Start XR session
+    xrHelper.baseExperience
+      .enterXRAsync("immersive-vr", "local-floor")
+      .then(() => {
+        console.log("Entered XR");
+      });
+  } catch (e) {
+    console.error("WebXR not supported or failed to enter:", e);
+  }
+});
 
 
 if (import.meta.hot) {
