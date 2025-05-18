15deeb07d8a72f42862bdb87955dbfe63bb24e0c
gongbaodd@ProArt <gongbaodd@qq.com>
2025-05-19 00:25:28 +0300

    fix debug

--- xr-project/src/main.tsx
+++ xr-project/src/main.tsx
@@ -16,16 +16,15 @@ import { DevUI } from '@iwer/devui';
 		xrDevice.installRuntime();
 		xrDevice.fovy = (75 / 180) * Math.PI;
 		xrDevice.ipd = 0;
-		window.xrdevice = xrDevice;
-		xrDevice.controllers.right.position.set(0.15649, 1.43474, -0.38368);
-		xrDevice.controllers.right.quaternion.set(
+		xrDevice.controllers.right?.position.set(0.15649, 1.43474, -0.38368);
+		xrDevice.controllers.right?.quaternion.set(
 			0.14766305685043335,
 			0.02471366710960865,
 			-0.0037767395842820406,
 			0.9887216687202454,
 		);
-		xrDevice.controllers.left.position.set(-0.15649, 1.43474, -0.38368);
-		xrDevice.controllers.left.quaternion.set(
+		xrDevice.controllers.left?.position.set(-0.15649, 1.43474, -0.38368);
+		xrDevice.controllers.left?.quaternion.set(
 			0.14766305685043335,
 			0.02471366710960865,
 			-0.0037767395842820406,
