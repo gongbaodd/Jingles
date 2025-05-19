9a5fb60beb818e8754c46aeac7a9d7851d3e34ae
gongbaodd@ProArt <gongbaodd@qq.com>
2025-05-19 09:59:42 +0300

    remove top awaits

--- xr-project/src/Scene.tsx
+++ xr-project/src/Scene.tsx
@@ -1,4 +1,4 @@
-import { ArcRotateCamera, EquiRectangularCubeTexture, HemisphericLight, ImportMeshAsync, MeshBuilder, PBRMaterial, Scene, Texture, Vector3, type Engine } from "@babylonjs/core";
+import { AbstractMesh, ArcRotateCamera, AssetsManager, EquiRectangularCubeTexture, HemisphericLight, MeshBuilder, PBRMaterial, Scene, Texture, Vector3, type Engine } from "@babylonjs/core";
 // import "@babylonjs/materials";
 
 export async function createScene(engine: Engine) {
@@ -19,22 +19,32 @@ export async function createScene(engine: Engine) {
     const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
     light.intensity = 0.7;
 
-    loadSky(scene);
-    LoadPointCloud(scene);
+    const assetManager = new AssetsManager(scene);
 
+    loadSky(scene, assetManager);
+    loadPointCloud(assetManager);
+
+    assetManager.load();
     return scene;
 }
+ 
+async function loadSky(scene: Scene, assetManager: AssetsManager) {
+    const textureP = new Promise<EquiRectangularCubeTexture>(res => {
+        const textureTask = assetManager.addEquiRectangularCubeTextureAssetTask(
+            "sky",
+            "./images/sky.jpg",
+            2048,
+        );
+        textureTask.onSuccess = task => {
+            task.texture.coordinatesMode = Texture.SKYBOX_MODE;
+            res(task.texture)
+        }
+    })
 
-function loadSky(scene: Scene) {
-    const eqTexture = new EquiRectangularCubeTexture(
-        "./images/sky.jpg",
-        scene,
-        128,
-    );
-    eqTexture.coordinatesMode = Texture.SKYBOX_MODE;
-
+    const eqTexture = await textureP;
     const hdrSkybox = MeshBuilder.CreateBox("hdrSkyBox", { size: 2000 }, scene);
     const hdrSkyboxMaterial = new PBRMaterial("skyBox", scene);
+
     hdrSkyboxMaterial.backFaceCulling = false;
     hdrSkyboxMaterial.reflectionTexture = eqTexture.clone();
     hdrSkyboxMaterial.reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE;
@@ -47,10 +57,23 @@ function loadSky(scene: Scene) {
     scene.imageProcessingConfiguration.contrast = 3.0;
 }
 
-async function LoadPointCloud(scene: Scene) {
-    const result = await ImportMeshAsync("models/logo.ply", scene);
+async function loadPointCloud(assetManager: AssetsManager) {
+    const meshP = new Promise<AbstractMesh[]>(res => {
+        const meshTask = assetManager.addMeshTask(
+            "logo",
+            "",
+            "./models/",
+            "logo.ply"
+        )
+        meshTask.onSuccess = task => {
+            res(task.loadedMeshes)
+        }
+    })
 
-    const mesh = result.meshes[0];
+    const result = await meshP;
+
+
+    const mesh = result[0];
     mesh.position.y = -50.0;
     mesh.rotation.z = Math.PI;
     mesh.rotation.y = -280/360 * Math.PI;
--- xr-project/src/main.tsx
+++ xr-project/src/main.tsx
@@ -1,4 +1,4 @@
-import { Engine } from '@babylonjs/core';
+import { Engine, Scene } from '@babylonjs/core';
 import './index.css'
 import { createScene } from './Scene';
 import "@babylonjs/inspector";
@@ -6,80 +6,83 @@ import { registerBuiltInLoaders } from "@babylonjs/loaders/dynamic"
 import { XRDevice, metaQuest3 } from 'iwer';
 import { DevUI } from '@iwer/devui';
 
-  	// iwer setup
-	let nativeWebXRSupport = false;
-	if (navigator.xr) {
-		nativeWebXRSupport = await navigator.xr.isSessionSupported('immersive-vr');
-	}
-	if (!nativeWebXRSupport) {
-		const xrDevice = new XRDevice(metaQuest3);
-		xrDevice.installRuntime();
-		xrDevice.fovy = (75 / 180) * Math.PI;
-		xrDevice.ipd = 0;
-		xrDevice.controllers.right?.position.set(0.15649, 1.43474, -0.38368);
-		xrDevice.controllers.right?.quaternion.set(
-			0.14766305685043335,
-			0.02471366710960865,
-			-0.0037767395842820406,
-			0.9887216687202454,
-		);
-		xrDevice.controllers.left?.position.set(-0.15649, 1.43474, -0.38368);
-		xrDevice.controllers.left?.quaternion.set(
-			0.14766305685043335,
-			0.02471366710960865,
-			-0.0037767395842820406,
-			0.9887216687202454,
-		);
-		new DevUI(xrDevice);
-	}
+let scene: Scene;
+let engine: Engine;
+async function init() {
+  // iwer setup
+  let nativeWebXRSupport = false;
+  if (navigator.xr) {
+    nativeWebXRSupport = await navigator.xr.isSessionSupported('immersive-vr');
+  }
+  if (!nativeWebXRSupport) {
+    const xrDevice = new XRDevice(metaQuest3);
+    xrDevice.installRuntime();
+    xrDevice.fovy = (75 / 180) * Math.PI;
+    xrDevice.ipd = 0;
+    xrDevice.controllers.right?.position.set(0.15649, 1.43474, -0.38368);
+    xrDevice.controllers.right?.quaternion.set(
+      0.14766305685043335,
+      0.02471366710960865,
+      -0.0037767395842820406,
+      0.9887216687202454,
+    );
+    xrDevice.controllers.left?.position.set(-0.15649, 1.43474, -0.38368);
+    xrDevice.controllers.left?.quaternion.set(
+      0.14766305685043335,
+      0.02471366710960865,
+      -0.0037767395842820406,
+      0.9887216687202454,
+    );
+    new DevUI(xrDevice);
+  }
 
-registerBuiltInLoaders();
+  registerBuiltInLoaders();
 
-const canvas = document.querySelector<HTMLCanvasElement>("#canvas")!;
+  const canvas = document.querySelector<HTMLCanvasElement>("#canvas")!;
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-const onResize = () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-};
+  const onResize = () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  };
 
-window.addEventListener("resize", onResize);
+  window.addEventListener("resize", onResize);
 
-const engine = new Engine(canvas, true, {
-  preserveDrawingBuffer: true,
-  stencil: true,
-  disableWebGL2Support: false,
-});
-let scene = await createScene(engine);
-engine.runRenderLoop(() => scene.render());
-
-
-document.getElementById("xr-button")!.addEventListener("click", async () => {
-  if (!scene) return;
-
+  engine = new Engine(canvas, true, {
+    preserveDrawingBuffer: true,
+    stencil: true,
+    disableWebGL2Support: false,
+  });
+  scene = await createScene(engine);
+  engine.runRenderLoop(() => scene.render());
 
 
+  document.getElementById("xr-button")!.addEventListener("click", async () => {
+    if (!scene) return;
 
-  try {
-    const xrHelper = await scene.createDefaultXRExperienceAsync({
-      uiOptions: {
-        sessionMode: "immersive-vr",
-      },
-      optionalFeatures: true,
-    });
-
-    // Start XR session
-    xrHelper.baseExperience
-      .enterXRAsync("immersive-vr", "local-floor")
-      .then(() => {
-        console.log("Entered XR");
+    try {
+      const xrHelper = await scene.createDefaultXRExperienceAsync({
+        uiOptions: {
+          sessionMode: "immersive-vr",
+        },
+        optionalFeatures: true,
       });
-  } catch (e) {
-    console.error("WebXR not supported or failed to enter:", e);
-  }
-});
 
+      // Start XR session
+      xrHelper.baseExperience
+        .enterXRAsync("immersive-vr", "local-floor")
+        .then(() => {
+          console.log("Entered XR");
+        });
+
+    } catch (e) {
+      console.error("WebXR not supported or failed to enter:", e);
+    }
+  });
+}
+
+init();
 
 if (import.meta.hot) {
   import.meta.hot.accept("./scene", async mod => {
